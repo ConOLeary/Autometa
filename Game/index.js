0f3887fab910f7c7c3e027be7a456d063cf075a4
ConOLeary <conolaoghaire@gmail.com>
2021-02-04 12:16:33 +0000

    First commit

---
+++ Game/index.js
@@ -0,0 +1,28 @@
+const canvas = document.querySelector('canvas')
+const c = canvas.getContext('2d')
+
+canvas.width = innerWidth
+canvas.heigh = innerHeight
+
+class Player {
+    constructor(x, y, radius, color) {
+        this.x = x
+        this.y = y
+        this.radius = radius
+        this.color = color
+    }
+    helloWorld(){
+
+    }
+    draw() {
+        c.beginPath()
+        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+        c.fillStyle = this.color
+        c.fill()
+    }
+}
+
+const player = new Player(100, 100, 30, 'blue')
+player.draw()
+
+console.log(player)
